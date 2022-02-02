e7288f276ca99ed3a26623de245398f25856fdd6
Minxuan Liu <realnimiliu@gmail.com>
2022-02-02 01:21:38 -0800

    Query result nodes in output tag in xml file generated; NO support for reading from input file

--- src/main/java/listener/main.java
+++ src/main/java/listener/main.java
@@ -7,34 +7,37 @@ import org.antlr.v4.runtime.ParserRuleContext;
 import org.antlr.v4.runtime.tree.ParseTreeWalker;
 import org.w3c.dom.Document;
 import DOMBuilder.DOMBuilder;
+import org.w3c.dom.Element;
 import org.w3c.dom.Node;
 
-import javax.xml.transform.Result;
-import javax.xml.transform.Source;
-import javax.xml.transform.Transformer;
-import javax.xml.transform.TransformerFactory;
+import javax.xml.XMLConstants;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.transform.*;
 import javax.xml.transform.dom.DOMSource;
 import javax.xml.transform.stream.StreamResult;
-import java.io.ByteArrayOutputStream;
-import java.io.IOException;
-import java.util.HashSet;
+import javax.xml.transform.stream.StreamSource;
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.util.List;
-import java.util.Set;
 
-//package listener;
-//
-//import org.antlr.v4.runtime.CharStreams;
-//import org.antlr.v4.runtime.CommonTokenStream;
-//import org.antlr.v4.runtime.ParserRuleContext;
-//import org.antlr.v4.runtime.tree.ParseTreeWalker;
-//
-//import edu.ucsd.CSE232B.parsers.ExpressionGrammarLexer;
-//import edu.ucsd.CSE232B.parsers.ExpressionGrammarParser;
-//import edu.ucsd.cse232b.antlrTutorial.expression.Prog;
-//
 public class main {
      public static void main(String[] args) throws Exception {
-         String expression = null;
+         String filename = "Input_Query.txt";
+         File inputQueryFile = new File(filename);
+         if(inputQueryFile.createNewFile()){
+             System.out.println("File " + filename + " created in Project root directory");
+         }else System.out.println("File " + filename + " already exists in the project root directory");
+
+         String expression = "doc(\"j_caesar.xml\")//(ACT,PERSONAE)/TITLE/text()/../../TITLE[not((./ACT)and(./ACT))]/.././PERSONA/./..";
+//                 Files.readString(Path.of(filename));
+//                 "doc(\"j_caesar.xml\")\u200B/PLAY/ACT[not(SCENE=ACT)]/TITLE/.";
+
+//         System.out.println("Expression to be tested: " + expression);
+         String output_filename = "Query_Results.xml";
+         test(expression, output_filename);
+
          //expression = "doc(\"j_caesar.xml\")//PERSONA";
          //test(expression);
 //         expression = "doc(\"j_caesar.xml\")//SCENE[SPEECH/SPEAKER/text() = \"CAESAR\"]/TITLE";
@@ -66,13 +69,22 @@ public class main {
 //         test(expression); //0
 //         expression = "doc(\"j_caesar.xml\")//(ACT/SCENE/SPEECH)/(SPEAKER)/../.[not (./../TITLE==./LINE) and (./../*/SPEAKER==./SPEAKER) ]/../TITLE";
 //         test(expression); //18
-
-
-
-
     }
 
-    public static void test(String expression) throws Exception {
+    public static void test(String expression, String output_filename) throws Exception {
+        // Output file
+        File queryResultsFile = new File(output_filename);
+        if(queryResultsFile.createNewFile()){
+            System.out.println("File " + queryResultsFile + " created in Project root directory");
+        }else System.out.println("File " + queryResultsFile + " already exists in the project root directory");
+
+        // Create an empty output tag/element
+        DocumentBuilderFactory documentBuilderFactory = DocumentBuilderFactory.newInstance();
+        DocumentBuilder documentBuilder = documentBuilderFactory.newDocumentBuilder();
+        Document document = documentBuilder.newDocument();
+        Element root = document.createElement("OUTPUT");
+
+        // Execute query
         ExpressionGrammarLexer lexer = new ExpressionGrammarLexer(CharStreams.fromString(expression));
         CommonTokenStream tokens = new CommonTokenStream(lexer);
         ExpressionGrammarParser parser = new ExpressionGrammarParser(tokens);
@@ -81,22 +93,21 @@ public class main {
         ExpressionBuilder expressionBuilder = new ExpressionBuilder();
         walker.walk(expressionBuilder, tree);
 
-        List<Node> document = expressionBuilder.getDocument(tree);
-        System.out.println("Result: " + String.valueOf(document.size()));
-        System.out.println(document);
-//        int i = 1;
-        for(Node d : document) {
-            //String xmlSource = nodeToString(d);
-//            String filename = "./XML_files/queryResult-" + i + ".xml";
-//            stringToDom(xmlSource, filename);
-//             DOMBuilder domBuilder = new DOMBuilder();
-//             domBuilder.echo(d);
-//             domBuilder.out.flush();
-//             System.out.println("h");
-//             domBuilder.out.close();
-            //System.out.println(d.getNodeName());
-//            i++;
+        // Debugging
+        List<Node> resultNodes = expressionBuilder.getDocument(tree);
+        System.out.println("Result: " + resultNodes.size());
+        System.out.println(resultNodes);
+
+        for(Node d : resultNodes) {
+            // Have to import d into the newly created output xml document
+            Node d_copy = document.importNode(d, true);
+            root.appendChild(d_copy);
+//            System.out.println(d.getNodeName());
         }
+        document.appendChild(root);
+        String xml_output_str = nodeToString(document);
+        xml_output_str = prettyFormat(xml_output_str);
+        Files.writeString(Path.of(output_filename), xml_output_str);
     }
 
     /**
@@ -107,17 +118,39 @@ public class main {
 
         Source xmlSource = new DOMSource(document);
         Result outputTarget = new StreamResult(outputStream);
-        Transformer transformer = TransformerFactory.newInstance()
-                .newTransformer();
+        Transformer transformer = TransformerFactory.newInstance().newTransformer();
         transformer.transform(xmlSource, outputTarget);
 
         return outputStream.toString();
     }
 
-    public static void stringToDom(String xmlSource, String filename)
-            throws IOException, IOException {
-        java.io.FileWriter fw = new java.io.FileWriter(filename);
-        fw.write(xmlSource);
-        fw.close();
+    // XML file indent: ugly one-line XML str -> pretty indented XML str
+    public static String prettyFormat(String input, int indent) {
+        try {
+            Source xmlInput = new StreamSource(new StringReader(input));
+            StringWriter stringWriter = new StringWriter();
+            StreamResult xmlOutput = new StreamResult(stringWriter);
+            TransformerFactory transformerFactory = TransformerFactory.newInstance();
+            transformerFactory.setAttribute("indent-number", indent);
+            transformerFactory.setAttribute(XMLConstants.ACCESS_EXTERNAL_DTD, "");
+            transformerFactory.setAttribute(XMLConstants.ACCESS_EXTERNAL_STYLESHEET, "");
+            Transformer transformer = transformerFactory.newTransformer();
+            transformer.setOutputProperty(OutputKeys.INDENT, "yes");
+            transformer.transform(xmlInput, xmlOutput);
+            return xmlOutput.getWriter().toString();
+        } catch (Exception e) {
+            throw new RuntimeException(e); // simple exception handling, please review it
+        }
     }
+
+    public static String prettyFormat(String input) {
+        return prettyFormat(input, 2);
+    }
+
+//    public static void stringToDom(String xmlSource, String filename)
+//            throws IOException, IOException {
+//        java.io.FileWriter fw = new java.io.FileWriter(filename);
+//        fw.write(xmlSource);
+//        fw.close();
+//    }
 }
